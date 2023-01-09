168993bc50ed5c73a14b703de67b4fccf77e1b67
Imam Ramadhan <imamrmdn99@gmail.com>
2023-01-10 00:02:13 +0700

    update: get data by id

--- src/student/controller.js
+++ src/student/controller.js
@@ -1,12 +1,27 @@
 const pool = require('../../config')
+const queries = require('./queries')
 
 const getStudents = (req, res) => {
-    pool.query("SELECT * FROM students", (error, results) => {
+    pool.query(queries.getStudents, (error, results) => {
         if (error) throw error;
         res.status(200).json(results.rows)
-    });
+    })
+}
+
+const getStudentById = (req, res) => {
+    const id =  parseInt(req.params.id)
+    pool.query(queries.getStudentById, [id], (error, result) => {
+        if (error) throw error;
+        res.status(200).json(result.rows)
+    })
+}
+
+const addStudent = (req, res) => {
+    const { name, email, age, dob} = req.body
 }
 
 module.exports = {
     getStudents,
+    getStudentById,
+    addStudent
 };
--- src/student/routes.js
+++ src/student/routes.js
@@ -4,5 +4,7 @@
  const router = Router()
 
  router.get('/', controller.getStudents)
+ router.post('/', controller.addStudent);
+ router.get('/:id', controller.getStudentById)
 
  module.exports = router
