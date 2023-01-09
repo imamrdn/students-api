9e63f7d11b63e4bd8f907418f28147d4afaa0433
Imam Ramadhan <imamrmdn99@gmail.com>
2023-01-10 06:17:40 +0700

    update: update student

--- src/student/routes.js
+++ src/student/routes.js
@@ -6,7 +6,7 @@
  router.get('/', controller.getStudents)
  router.post('/', controller.addStudent)
  router.get('/:id', controller.getStudentById)
- 
+ router.put('/:id', controller.updateStudent)
  router.delete('/:id', controller.removeStudent)
  
 
