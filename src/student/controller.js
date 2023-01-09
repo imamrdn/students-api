b083d1c90ba7dd7943904abe6ac871e0f1b53760
Imam Ramadhan <imamrmdn99@gmail.com>
2023-01-10 05:32:53 +0700

    update: create student

--- src/student/controller.js
+++ src/student/controller.js
@@ -10,7 +10,7 @@ const getStudents = (req, res) => {
 
 const getStudentById = (req, res) => {
     const id =  parseInt(req.params.id)
-    pool.query(queries.getStudentById, [id], (error, result) => {
+    pool.query(queries.getStudentById, [id], (error, results) => {
         if (error) throw error;
         res.status(200).json(result.rows)
     })
@@ -18,6 +18,17 @@ const getStudentById = (req, res) => {
 
 const addStudent = (req, res) => {
     const { name, email, age, dob} = req.body
+    pool.query(queries.checkEmailExists, [email], (error, results) => {
+        if (results.rows.length) {
+            res.send("Email already exists");
+        }
+
+        //add student
+        pool.query(queries.addStudent, [name, email, age, dob], (error, results) => {
+            if (error) throw error
+            res.status(201).send("Student Created Successfully")
+        }) 
+    })
 }
 
 module.exports = {
