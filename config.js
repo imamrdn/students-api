fde6bde45b39eb1859b2e5860f87ee8106b7c11a
Imam Ramadhan <imamrmdn99@gmail.com>
2023-01-09 23:21:53 +0700

    update: get data all student

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
-const Pool = require('pg')
+const Pool = require('pg').Pool
 const pool = new Pool ({
     user: "postgres",
     host: "localhost",
     database: "students",
     password: "postgres",
     port: 5432,
-})
+});
 
-module.exports = pool
+module.exports = pool;
--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express')
+const studentsRoutes = require('./src/student/routes')
+
 const app = express()
 const port = 3100
+
+app.use(express.json())
 
 app.get("/", (req, res ) => {
     res.send("Hello")
 })
+
+app.use('/api/v1/students', studentsRoutes)
 
 app.listen(port, () => console.log(`app listening on port ${port}`));
 
